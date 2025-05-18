33ce4d588655e4b4ffff1676a5932434c9e7ff9e
Miruna Lorent <lorentmiruna1@gmail.com>
2025-05-18 18:15:57 +0300

    Added in lib : functions for task4

--- gamelib.h
+++ gamelib.h
@@ -24,6 +24,15 @@ struct copac{
 };
 typedef struct copac TreeNode;
 
+typedef struct Ngraph{
+    int l,c;
+}Nodegraph;
+
+typedef struct graphs{
+    Nodegraph *v;
+    int V; //nr of nodes
+    int conex; //0 or 1
+}graph;
 
 int calculare_vecini(char *matrice,int N,int M,int i,int j);
 
@@ -61,6 +70,8 @@ void parcurgere(TreeNode *root,const char* matrice,int N,int M,int lvl,int max);
 
 void listingTreeM(TreeNode* root, char* matrice, int N, int M,FILE* text1, int lvl);
 
+void deleteGraph(graph** g);
+
 void deleteTree(TreeNode** root);
 
 void next_matrice(FILE *text1,char *matrice,int N,int M);
@@ -68,3 +79,21 @@ void next_matrice(FILE *text1,char *matrice,int N,int M);
 void pop(Stack **top);
 
 void printStack(Stack *top);
+
+graph* creategraph(char* m,int N,int M);
+
+int isconnected(Nodegraph a,Nodegraph b);
+
+int**  adjency_matrix(graph *g);
+
+void DFS_scan(int**m,int V,int visited[],int i);
+
+int DFS(int**m,int V,int* comp);
+
+int is_best(int path[], int bestpath[], int n, graph* g);
+
+void dfsPath(int** m, int V, int node, int* visited, int* path, int n, int* bestn, int* bestpath,graph* g);
+
+void getLongestPath(int** m, int V, graph* g,int comp_conexe,int* comp);
+
+int* copyINTarray(int* copy,int* original,int V);
